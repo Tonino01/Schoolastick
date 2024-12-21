69934a2137ef1f4e4bf54c58283ecd6dc93ecc61
daniel tonin <122627482+Tonino01@users.noreply.github.com>
2024-12-21 20:26:49 +0100

    finito funzionamento js
    , inizziato crea segnalazione

--- Js/docenteScript.js
+++ Js/docenteScript.js
@@ -1,6 +1,7 @@
 
 // Funzione per caricare il file HTML esterno
-async function caricaSegnalazioni() {
+async function mostraSegnalazioni() {
+
     try {
         const response = await fetch('librerie/mostraSegnalazioni.html');
         if (!response.ok) {
@@ -19,4 +20,45 @@ async function caricaSegnalazioni() {
     }
 }
 
-caricaSegnalazioni();
+async function creaSegnalazioni(){
+
+
+  try {
+      const response = await fetch('librerie/creaSegnalazioni.html');
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const html = await response.text(); // Ottieni il contenuto del file come testo
+      // Salva il contenuto in una variabile
+      const codiceHTML = html;
+
+      // Puoi anche inserire il codice HTML in un elemento della pagina
+      document.getElementById('contenitore').innerHTML = codiceHTML;
+
+      console.log(codiceHTML); // Stampa il codice HTML nella console
+  } catch (error) {
+      console.error('Si è verificato un errore:', error);
+  }
+
+}
+
+
+function pulisciContenitore(){
+
+  document.getElementById("contenitore").innerHTML = '';
+
+}
+
+function segnalazioni(){
+
+  pulisciContenitore();
+  mostraSegnalazioni();
+
+}
+
+function nuovaSegnalazione(){
+
+  pulisciContenitore();
+  creaSegnalazioni();
+
+}
