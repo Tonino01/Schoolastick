77996748cd37d3ebdae4d6aed16cad4eba2fe85e
daniel tonin <122627482+Tonino01@users.noreply.github.com>
2024-12-23 16:46:23 +0100

    general update

--- Js/docenteScript.js
+++ Js/docenteScript.js
@@ -22,9 +22,29 @@ async function mostraSegnalazioni() {
 
 async function creaSegnalazioni(){
 
-
   try {
       const response = await fetch('librerie/creaSegnalazioni.html');
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const html = await response.text(); // Ottieni il contenuto del file come testo
+      // Salva il contenuto in una variabile
+      const codiceHTML = html;
+
+      // Puoi anche inserire il codice HTML in un elemento della pagina
+      document.getElementById('contenitore').innerHTML = codiceHTML;
+
+      console.log(codiceHTML); // Stampa il codice HTML nella console
+  } catch (error) {
+      console.error('Si è verificato un errore:', error);
+  }
+
+}
+
+async function infoAccount(){
+
+  try {
+      const response = await fetch('librerie/infoAccount.html');
       if (!response.ok) {
           throw new Error('Network response was not ok');
       }
@@ -60,5 +80,12 @@ function nuovaSegnalazione(){
 
   pulisciContenitore();
   creaSegnalazioni();
+
+}
+
+function mostraInfoAccount(){
+
+  pulisciContenitore();
+  infoAccount();
 
 }
