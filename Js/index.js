0c06548c48241e7d294ffa154eaa6659d0d87bc7
daniel tonin <122627482+Tonino01@users.noreply.github.com>
2025-04-17 22:32:59 +0200

    modificato index con NetStrike

--- Js/index.js
+++ Js/index.js
@@ -1,15 +1,26 @@
 const infoBtn = document.getElementById('info-btn');
 const logoBtn = document.getElementById('logo-btn');
+const netStrikeBtn = document.getElementById('NetStrike-btn'); 
 const infoBox = document.getElementById('info-box');
+const netStrikeBox = document.getElementById('NetStrike-box'); 
 const mainContent = document.getElementById('main-content');
 const welcomeContent = document.getElementById('welcome-content');
 
 infoBtn.addEventListener('click', () => {
     welcomeContent.style.display = 'none';
     infoBox.classList.add('show');
+    netStrikeBox.classList.remove('show'); 
 });
 
 logoBtn.addEventListener('click', () => {
     welcomeContent.style.display = 'block';
     infoBox.classList.remove('show');
+    netStrikeBox.classList.remove('show'); 
+});
+
+netStrikeBtn.addEventListener('click', () => { 
+    
+    welcomeContent.style.display = 'none';
+    netStrikeBox.classList.add('show');
+    infoBox.classList.remove('show'); 
 });
